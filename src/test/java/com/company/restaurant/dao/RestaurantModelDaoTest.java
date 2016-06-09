208c7441983f1b4f8189a5c6f5a6d8dab412893f
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-09 09:53:21 +0300

    Restaurant.Model.Dao developing

--- src/test/java/com/company/restaurant/dao/RestaurantModelDaoTest.java
+++ src/test/java/com/company/restaurant/dao/RestaurantModelDaoTest.java
@@ -15,6 +15,8 @@ import static org.junit.Assert.assertTrue;
  * Created by Yevhen on 08.06.2016.
  */
 public abstract class RestaurantModelDaoTest {
+    private final static String DUPLICATE_KEY_VALUE_VIOLATES_MESSAGE = "duplicate key value violates";
+
     private static JobPositionDao jobPositionDao;
     private static EmployeeDao employeeDao;
     private static MenuDao menuDao;
@@ -152,7 +154,7 @@ public abstract class RestaurantModelDaoTest {
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-        initDataSource(null); // Intentionally, to generate exception if use this code directly
+        initDataSource(null); // intentionally, to generate exception if use this code directly
 
         initEnvironment();
     }
@@ -184,7 +186,6 @@ public abstract class RestaurantModelDaoTest {
             System.out.println("Job position Id :" + jobPosition.getId() +
                     ", Job position name :" + jobPosition.getName());
         }
-
 
         String firstName = Util.getRandomString();
         String secondName = Util.getRandomString();
@@ -321,9 +322,21 @@ public abstract class RestaurantModelDaoTest {
     @Test(timeout = 2000)
     public void addFindDelTableTest() throws Exception {
         Table table = new Table();
-        table.setNumber(tableDao.findTableById(lastTableId()).getNumber() + Util.getRandomInteger());
         table.setDescription(Util.getRandomString());
-        table = tableDao.addTable(table);
+        boolean tableWasNotAdded = true;
+        do {
+            try {
+                table.setNumber(tableDao.findTableById(lastTableId()).getNumber() + Util.getRandomInteger());
+                table = tableDao.addTable(table);
+                tableWasNotAdded = false;
+            } catch (RuntimeException e) {
+                //  Error "duplicate key value violates unique constraint "ak_u_table_number_table"" could be generated
+                if (!e.getMessage().contains(DUPLICATE_KEY_VALUE_VIOLATES_MESSAGE)) {
+                    throw new RuntimeException(e);
+
+                }
+            }
+        } while (tableWasNotAdded);
 
         assertTrue(ObjectService.isEqualByGetterValuesStringRepresentation(table,
                 tableDao.findTableByNumber(table.getNumber())));
