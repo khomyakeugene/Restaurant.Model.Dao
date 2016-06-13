1816ae09823d11b1e899c17d83ba58e095b6877e
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-13 16:54:26 +0300

    Restaurant.Model.Dao developing

---
+++ src/main/java/com/company/restaurant/model/OrderCourse.java
@@ -0,0 +1,30 @@
+package com.company.restaurant.model;
+
+/**
+ * Created by Yevhen on 13.06.2016.
+ */
+public class OrderCourse extends IntegerLinkObject  {
+    public int getCourseId() {
+        return getFirstId();
+    }
+
+    public void setCourseId(int courseId) {
+        setFirstId(courseId);
+    }
+
+    public int getOrderId() {
+        return getSecondId();
+    }
+
+    public void setOrderId(int orderId) {
+        setSecondId(orderId);
+    }
+
+    public int getCourseQuantity() {
+        return getIntegerLinkData();
+    }
+
+    public void setCourseQuantity(int courseQuantity) {
+        setIntegerLinkData(courseQuantity);
+    }
+}
