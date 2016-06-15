7c2752282d1e21cdf81e2136898633b3fe9c87b7
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-15 22:06:56 +0300

    Restaurant.Model.Dao developing

--- src/main/java/com/company/restaurant/model/Ingredient.java
+++ src/main/java/com/company/restaurant/model/Ingredient.java
@@ -4,11 +4,11 @@ package com.company.restaurant.model;
  * Created by Yevhen on 24.05.2016.
  */
 public class Ingredient extends SimpleDic {
-    public int getIngredient() {
+    public int getIngredientId() {
         return getId();
     }
 
-    public void setIngredient(int ingredientId) {
+    public void setIngredientId(int ingredientId) {
         setId(ingredientId);
     }
 }
