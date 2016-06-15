f58640d46e83ce4632a9c762a0089cfea9f62e2d
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-16 00:16:00 +0300

    Restaurant.Model.Dao developing

--- src/main/java/com/company/restaurant/dao/WarehouseViewDao.java
+++ src/main/java/com/company/restaurant/dao/WarehouseViewDao.java
@@ -2,23 +2,23 @@ package com.company.restaurant.dao;
 
 import com.company.restaurant.model.Ingredient;
 import com.company.restaurant.model.Portion;
-import com.company.restaurant.model.Warehouse;
+import com.company.restaurant.model.WarehouseView;
 
 import java.util.List;
 
 /**
  * Created by Yevhen on 24.05.2016.
  */
-public interface WarehouseDao {
+public interface WarehouseViewDao {
     void addIngredientToWarehouse(Ingredient ingredient, Portion portion, float amount);
 
     void takeIngredientFromWarehouse(Ingredient ingredient, Portion portion, float amount);
 
-    List<Warehouse> findIngredientInWarehouseByName(String name);
+    List<WarehouseView> findIngredientInWarehouseByName(String name);
 
-    List<Warehouse> findIngredientInWarehouseById(int ingredientId);
+    List<WarehouseView> findIngredientInWarehouseById(int ingredientId);
 
-    List<Warehouse> findAllWarehouseIngredients();
+    List<WarehouseView> findAllWarehouseIngredients();
 
-    List<Warehouse> findAllElapsingWarehouseIngredients(float limit);
+    List<WarehouseView> findAllElapsingWarehouseIngredients(float limit);
 }
--- src/main/java/com/company/restaurant/model/WarehouseView.java
+++ src/main/java/com/company/restaurant/model/WarehouseView.java
@@ -3,7 +3,7 @@ package com.company.restaurant.model;
 /**
  * Created by Yevhen on 24.05.2016.
  */
-public class Warehouse extends FloatLinkObject {
+public class WarehouseView extends FloatLinkObject {
     private String ingredientName;
     private int portionTypeId;
     private int measuringTypeId;
