735e7a03b538bb366b26a46cde2d26ddc7a46720
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-26 03:41:11 +0300

    Restaurant.Model.Dao developing

--- src/main/java/com/company/restaurant/model/State.java
+++ src/main/java/com/company/restaurant/model/State.java
@@ -4,4 +4,11 @@ package com.company.restaurant.model;
  * Created by Yevhen on 25.06.2016.
  */
 public class State extends SimpleTypeDic {
+    String getStateType() {
+        return getType();
+    }
+
+    void setStateType(String stateType) {
+        setType(stateType);
+    }
 }
