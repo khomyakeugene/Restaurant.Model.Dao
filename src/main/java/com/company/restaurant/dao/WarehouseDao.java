aaac4e6009b423a2515e56e0a94c723356c7336a
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-28 14:04:07 +0300

    Restaurant.Model.Dao developing

--- src/main/java/com/company/restaurant/dao/WarehouseDao.java
+++ src/main/java/com/company/restaurant/dao/WarehouseDao.java
@@ -2,6 +2,9 @@ package com.company.restaurant.dao;
 
 import com.company.restaurant.model.Ingredient;
 import com.company.restaurant.model.Portion;
+import com.company.restaurant.model.Warehouse;
+
+import java.util.List;
 
 /**
  * Created by Yevhen on 16.06.2016.
@@ -10,4 +13,12 @@ public interface WarehouseDao {
     void addIngredientToWarehouse(Ingredient ingredient, Portion portion, float amount);
 
     void takeIngredientFromWarehouse(Ingredient ingredient, Portion portion, float amount);
+
+    List<Warehouse> findIngredientInWarehouseByName(String name);
+
+    List<Warehouse> findIngredientInWarehouseById(int ingredientId);
+
+    List<Warehouse> findAllWarehouseIngredients();
+
+    List<Warehouse> findAllElapsingWarehouseIngredients(float limit);
 }
--- src/main/java/com/company/restaurant/model/Warehouse.java
+++ src/main/java/com/company/restaurant/model/Warehouse.java
@@ -6,6 +6,9 @@ import com.company.restaurant.model.proto.FloatLinkObject;
  * Created by Yevhen on 15.06.2016.
  */
 public class Warehouse extends FloatLinkObject {
+    private Ingredient ingredient = new Ingredient();
+    private Portion portion = new Portion();
+/*
     public int getIngredientId() {
         return getFirstId();
     }
@@ -21,12 +24,50 @@ public class Warehouse extends FloatLinkObject {
     public void setPortionId(int portionId) {
         setSecondId(portionId);
     }
-
+*/
     public Float getAmount() {
         return getFloatLinkData();
     }
 
     public void setAmount(Float amount) {
         setFloatLinkData(amount);
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Portion getPortion() {
+        return portion;
+    }
+
+    public void setPortion(Portion portion) {
+        this.portion = portion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Warehouse)) return false;
+        if (!super.equals(o)) return false;
+
+        Warehouse warehouse = (Warehouse) o;
+
+        return ingredient != null ? ingredient.equals(warehouse.ingredient) :
+                warehouse.ingredient == null && (portion != null ? portion.equals(warehouse.portion) :
+                        warehouse.portion == null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (ingredient != null ? ingredient.hashCode() : 0);
+        result = 31 * result + (portion != null ? portion.hashCode() : 0);
+        return result;
     }
 }
