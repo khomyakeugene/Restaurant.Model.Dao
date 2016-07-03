3a527d9c748931d72354fb446868229a3610febd
khomyakeugene <khomyak_eugene@yahoo.com>
2016-07-03 13:41:02 +0300

    Restaurant.Model.Dao developing

--- src/main/java/com/company/restaurant/model/CookAndWaiter.java
+++ src/main/java/com/company/restaurant/model/CookAndWaiter.java
@@ -32,4 +32,35 @@ public class CookAndWaiter extends Employee {
     public void setOrders(Set<Order> orders) {
         waiterProperty.setOrders(orders);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CookAndWaiter)) return false;
+        if (!super.equals(o)) return false;
+
+        CookAndWaiter that = (CookAndWaiter) o;
+
+        return cookProperty != null ? cookProperty.equals(that.cookProperty) :
+                that.cookProperty == null && (waiterProperty != null ?
+                        waiterProperty.equals(that.waiterProperty) : that.waiterProperty == null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (cookProperty != null ? cookProperty.hashCode() : 0);
+        result = 31 * result + (waiterProperty != null ? waiterProperty.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CookAndWaiter{" +
+                super.toString() + "\n" +
+                "cookProperty=" + cookProperty +
+                ", waiterProperty=" + waiterProperty +
+                '}';
+    }
 }
